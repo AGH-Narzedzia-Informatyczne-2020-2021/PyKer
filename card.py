88f5fbc1624e585d942052acfacad87146aaacb8
Kamil Poniewierski <kamilponiewierski98@gmail.com>
2020-11-01 21:32:19 +0100

    moved enums up so that the types can be used in card constructor

--- card.py
+++ card.py
@@ -1,24 +1,11 @@
 from enum import Enum
 
 
-class Card:
-    def __init__(self, suit, rank):
-        if not isinstance(suit, Suit):
-            raise TypeError('suit must be of type Suit')
-        if not isinstance(rank, Rank):
-            raise TypeError('rank must be of type Rank')
-        self.suit = suit
-        self.rank = rank
-
-    def __str__(self):
-        return str(self.rank).split('.')[1] + ' OF ' + str(self.suit).split('.')[1]
-
-
 class Suit(Enum):
-    HEARTS = 1
-    DIAMONDS = 2
-    CLUBS = 3
-    SPADES = 4
+    HEARTS = 0
+    DIAMONDS = 1
+    CLUBS = 2
+    SPADES = 3
 
 
 class Rank(Enum):
@@ -35,3 +22,16 @@ class Rank(Enum):
     FOUR = 4
     THREE = 3
     TWO = 2
+
+
+class Card:
+    def __init__(self, suit: Suit, rank: Rank):
+        if not isinstance(suit, Suit):
+            raise TypeError('suit must be of type Suit')
+        if not isinstance(rank, Rank):
+            raise TypeError('rank must be of type Rank')
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return str(self.rank).split('.')[1] + ' OF ' + str(self.suit).split('.')[1]
