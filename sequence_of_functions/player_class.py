42e277c2275a09f018b40fb14f7df85535570ae4
zarebajakub <73066603+zarebajakub@users.noreply.github.com>
2020-11-12 16:07:36 +0100

    Create player_class.py

---
+++ sequence_of_functions/player_class.py
@@ -0,0 +1,8 @@
+class Gracze():
+    def __init__(self):
+        self.name = input("Podaj imie gracza: ")
+        self.money = 100
+
+class Boty():
+    def __init__(self):
+        self.name = input("Wprowadz imie bota: ") #do zrobienia dynamiczną numeracje przy wpisywaniu
