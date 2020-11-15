5b46e67493adeaa9f388d1bdfc363d3b1df5fbdf
Kamil Poniewierski <kamilponiewierski98@gmail.com>
2020-11-15 19:01:57 +0100

    add methods for checking if the hand is a straight, flush or straight flush along with unittests for these

--- hand_evaluation/hand_evaluator.py
+++ hand_evaluation/hand_evaluator.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from card import Card
+from card import Card, Suit
 from hand_evaluation.evaluation import Evaluation
 
 
@@ -10,3 +10,48 @@ class HandEvaluator:
     def value_hand(hand: List[Card]) -> Evaluation:
         # TODO
         return 0
+
+    @staticmethod
+    def check_for_straight(hand: List[Card]) -> bool:
+        sorted_hand = sorted(hand, key=lambda ele: ele.rank.value)
+        if len(sorted_hand) < 5:
+            return False
+
+        streak = 1
+
+        for i in range(len(sorted_hand)):
+            if i > 0 and abs(sorted_hand[i].rank.value - sorted_hand[i - 1].rank.value) == 1:
+                streak += 1
+                if streak == 5:
+                    return True
+            else:
+                streak = 1
+
+        return streak == 5
+
+    @staticmethod
+    def check_for_flush(hand: List[Card]) -> bool:
+        suits = list(Suit)
+        for suit in suits:
+            suit_count = 0
+            for card in hand:
+                if card.suit == suit:
+                    suit_count += 1
+
+            if suit_count == 5:
+                return True
+
+        return False
+
+    @staticmethod
+    def check_for_straight_flush(hand: List[Card]) -> bool:
+        is_flush = HandEvaluator.check_for_flush(hand)
+        if not is_flush:
+            return False
+
+        assert (len(hand) <= 7)
+        sorted_by_suit = sorted(hand, key=lambda ele: ele.suit.value)
+        suit = sorted_by_suit[(len(sorted_by_suit) // 2)].suit
+
+        only_suit_cards = list(filter(lambda e: e.suit == suit, sorted_by_suit))
+        return HandEvaluator.check_for_straight(only_suit_cards)
--- hand_evaluation/hand_evaluator_test.py
+++ hand_evaluation/hand_evaluator_test.py
@@ -20,6 +20,38 @@ class MyTestCase(unittest.TestCase):
         hand_two = [Card(Suit.CLUBS, Rank.NINE), Card(Suit.DIAMONDS, Rank.NINE)]
         self.assertNotEqual(HandEvaluator.value_hand(hand_one), HandEvaluator.value_hand(hand_two))
 
+    # Straight tests
+
+    def test_find_straight(self):
+        self.assertTrue(HandEvaluator.check_for_straight(straight))
+        self.assertTrue(HandEvaluator.check_for_straight(straight_flush))
+
+    def test_find_no_straight(self):
+        self.assertFalse(HandEvaluator.check_for_straight(flush))
+
+    # Flush tests
+
+    def test_find_flush(self):
+        self.assertTrue(HandEvaluator.check_for_flush(flush))
+        self.assertTrue(HandEvaluator.check_for_flush(straight_flush))
+
+    def test_check_if_not_flush(self):
+        self.assertFalse(HandEvaluator.check_for_flush(straight))
+
+    def test_find_royal_flush(self):
+        self.assertTrue(HandEvaluator.check_for_straight_flush(straight_flush))
+
 
 if __name__ == '__main__':
     unittest.main()
+
+straight_flush = [Card(Suit.SPADES, Rank.TEN), Card(Suit.SPADES, Rank.EIGHT),
+                  Card(Suit.SPADES, Rank.NINE),
+                  Card(Suit.SPADES, Rank.JACK), Card(Suit.SPADES, Rank.QUEEN)]
+
+flush = [Card(Suit.SPADES, Rank.TEN), Card(Suit.SPADES, Rank.ACE), Card(Suit.SPADES, Rank.KING),
+         Card(Suit.SPADES, Rank.TWO), Card(Suit.SPADES, Rank.FIVE), Card(Suit.DIAMONDS, Rank.QUEEN),
+         Card(Suit.DIAMONDS, Rank.THREE)]
+
+straight = [Card(Suit.DIAMONDS, Rank.TEN), Card(Suit.HEARTS, Rank.NINE), Card(Suit.CLUBS, Rank.EIGHT),
+            Card(Suit.SPADES, Rank.SEVEN), Card(Suit.CLUBS, Rank.SIX), Card(Suit.DIAMONDS, Rank.ACE)]
