9a66c5765f139fb19594419c75eae3fb9085a4a2
zarebajakub <73066603+zarebajakub@users.noreply.github.com>
2020-11-19 13:46:50 +0100

    changed names of classes to english

--- sequence_of_functions/player_class.py
+++ sequence_of_functions/player_class.py
@@ -1,8 +1,8 @@
-class Gracze():
+class Players():
     def __init__(self):
         self.name = input("Podaj imie gracza: ")
         self.money = 100
 
-class Boty():
+class Bots():
     def __init__(self):
         self.name = input("Wprowadz imie bota: ") #do zrobienia dynamiczną numeracje przy wpisywaniu
