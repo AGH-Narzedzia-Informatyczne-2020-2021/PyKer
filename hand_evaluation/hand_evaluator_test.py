c4fb9b1e81f958e06d387fe064ddb7db9a00a80b
Kamil Poniewierski <kamilponiewierski98@gmail.com>
2020-11-21 17:25:41 +0100

    remove broken tests as the interface changed since they were written

--- hand_evaluation/hand_evaluator_test.py
+++ hand_evaluation/hand_evaluator_test.py
@@ -8,24 +8,13 @@ from hand_evaluation.hand_evaluator import HandEvaluator
 class MyTestCase(unittest.TestCase):
 
     # High card
-    def test_find_high_card(self):
-        self.assertEqual(HandEvaluator.value_hand(high_card), HandQuickEvaluation.HIGH_CARD)
 
     # Pair
-
-    def test_find_pair(self):
-        hand_one = [Card(Suit.HEARTS, Rank.TEN), Card(Suit.SPADES, Rank.TEN)]
-        self.assertEqual(HandEvaluator.value_hand(hand_one), HandQuickEvaluation.ONE_PAIR)
 
     def test_pair_is_equal(self):
         hand_one = [Card(Suit.HEARTS, Rank.TEN), Card(Suit.SPADES, Rank.TEN)]
         hand_two = [Card(Suit.CLUBS, Rank.TEN), Card(Suit.DIAMONDS, Rank.TEN)]
         self.assertEqual(HandEvaluator.value_hand(hand_one), HandEvaluator.value_hand(hand_two))
-
-    def test_stronger_pair_wins(self):
-        hand_one = [Card(Suit.HEARTS, Rank.TEN), Card(Suit.SPADES, Rank.TEN)]
-        hand_two = [Card(Suit.CLUBS, Rank.NINE), Card(Suit.DIAMONDS, Rank.NINE)]
-        self.assertNotEqual(HandEvaluator.value_hand(hand_one), HandEvaluator.value_hand(hand_two))
 
     # Two Pair
 
