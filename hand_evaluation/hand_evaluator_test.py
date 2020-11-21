524e2493907bbdb2d7cea390b97819a68d9f1d4c
Kamil Poniewierski <kamilponiewierski98@gmail.com>
2020-11-21 17:22:32 +0100

    update tests to use the new card constructor

--- hand_evaluation/hand_evaluator_test.py
+++ hand_evaluation/hand_evaluator_test.py
@@ -6,9 +6,16 @@ from hand_evaluation.hand_evaluator import HandEvaluator
 
 
 class MyTestCase(unittest.TestCase):
+
+    # High card
+    def test_find_high_card(self):
+        self.assertEqual(HandEvaluator.value_hand(high_card), HandQuickEvaluation.HIGH_CARD)
+
+    # Pair
+
     def test_find_pair(self):
         hand_one = [Card(Suit.HEARTS, Rank.TEN), Card(Suit.SPADES, Rank.TEN)]
-        self.assertEqual(HandEvaluator.value_hand(hand_one).quick_eval, HandQuickEvaluation.ONE_PAIR)
+        self.assertEqual(HandEvaluator.value_hand(hand_one), HandQuickEvaluation.ONE_PAIR)
 
     def test_pair_is_equal(self):
         hand_one = [Card(Suit.HEARTS, Rank.TEN), Card(Suit.SPADES, Rank.TEN)]
@@ -20,7 +27,11 @@ class MyTestCase(unittest.TestCase):
         hand_two = [Card(Suit.CLUBS, Rank.NINE), Card(Suit.DIAMONDS, Rank.NINE)]
         self.assertNotEqual(HandEvaluator.value_hand(hand_one), HandEvaluator.value_hand(hand_two))
 
-    # Straight tests
+    # Two Pair
+
+    # Three of a kind
+
+    # Straight
 
     def test_find_straight(self):
         self.assertTrue(HandEvaluator.check_for_straight(straight))
@@ -29,7 +40,7 @@ class MyTestCase(unittest.TestCase):
     def test_find_no_straight(self):
         self.assertFalse(HandEvaluator.check_for_straight(flush))
 
-    # Flush tests
+    # Flush
 
     def test_find_flush(self):
         self.assertTrue(HandEvaluator.check_for_flush(flush))
@@ -41,17 +52,31 @@ class MyTestCase(unittest.TestCase):
     def test_find_royal_flush(self):
         self.assertTrue(HandEvaluator.check_for_straight_flush(straight_flush))
 
+    # Four of a kind
+
 
 if __name__ == '__main__':
     unittest.main()
 
-straight_flush = [Card(Suit.SPADES, Rank.TEN), Card(Suit.SPADES, Rank.EIGHT),
-                  Card(Suit.SPADES, Rank.NINE),
-                  Card(Suit.SPADES, Rank.JACK), Card(Suit.SPADES, Rank.QUEEN)]
 
-flush = [Card(Suit.SPADES, Rank.TEN), Card(Suit.SPADES, Rank.ACE), Card(Suit.SPADES, Rank.KING),
-         Card(Suit.SPADES, Rank.TWO), Card(Suit.SPADES, Rank.FIVE), Card(Suit.DIAMONDS, Rank.QUEEN),
-         Card(Suit.DIAMONDS, Rank.THREE)]
+def str_to_card(card_string: str):
+    return Card(string=card_string)
 
-straight = [Card(Suit.DIAMONDS, Rank.TEN), Card(Suit.HEARTS, Rank.NINE), Card(Suit.CLUBS, Rank.EIGHT),
-            Card(Suit.SPADES, Rank.SEVEN), Card(Suit.CLUBS, Rank.SIX), Card(Suit.DIAMONDS, Rank.ACE)]
+
+def list_to_cards(card_list):
+    return list(map(str_to_card, card_list))
+
+
+straight_flush = list_to_cards(['10s', '8s', '9s', 'Js', 'Qs'])
+
+flush = list_to_cards(['10s', 'As', 'Ks', '2s', '5s', 'Qd', '3d'])
+
+straight = list_to_cards(['10d', '9h', '8c', '7s', '6c', 'Ad'])
+
+one_pair = list_to_cards(['8s', '8d', 'Ad', 'Jc'])
+
+two_pair = one_pair.__add__([Card(string='Ah')])
+
+full_house = two_pair.__add__([Card(string='As')])
+
+high_card = list_to_cards(['Ad', 'Qh', '10c', '2d', '5d'])
