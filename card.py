1ea5b5cd723a57f3ab2925a9a820a1757f473d6c
Kamil Poniewierski <kamilponiewierski98@gmail.com>
2020-11-21 17:22:05 +0100

    add possibility of creating a card with string

--- card.py
+++ card.py
@@ -41,13 +41,48 @@ class Rank(Enum):
 
 
 class Card:
-    def __init__(self, suit: Suit, rank: Rank):
-        if not isinstance(suit, Suit):
-            raise TypeError('suit must be of type Suit')
-        if not isinstance(rank, Rank):
-            raise TypeError('rank must be of type Rank')
-        self.suit = suit
-        self.rank = rank
+    def __init__(self, suit: Suit = None, rank: Rank = None, string: str = ''):
+
+        if string != '':
+            suit = string[-1]
+            rank = string[:-1]
+            self.suit = Card._str_to_suit(suit)
+            self.rank = Card._str_to_rank(rank)
+        else:
+            if not isinstance(suit, Suit):
+                raise TypeError('suit must be of type Suit')
+            if not isinstance(rank, Rank):
+                raise TypeError('rank must be of type Rank')
+            assert(Suit is not None)
+            assert(Rank is not None)
+
+            self.suit = suit
+            self.rank = rank
+
+    # TODO maybe move these to suit and rank classes
+
+    @staticmethod
+    def _str_to_suit(string: str):
+        if not ['h', 'd', 'c', 's'].__contains__(string.lower()):
+            raise ValueError(f'could not resolve suit: {string}')
+        suits = {'h': Suit.HEARTS, 'd': Suit.DIAMONDS, 'c': Suit.CLUBS, 's': Suit.SPADES}
+        return suits[string.lower()]
+
+    @staticmethod
+    def _str_to_rank(string: str):
+        if len(string) == 2:
+            if string != '10':
+                raise ValueError(f'could not resolve suit: {string}')
+            return Rank.TEN
+
+        if len(string) == 1:
+            ranks = {'A': Rank.ACE, 'K': Rank.KING, 'Q': Rank.QUEEN, 'J': Rank.JACK, '9': Rank.NINE, '8': Rank.EIGHT,
+                     '7': Rank.SEVEN, '6': Rank.SIX, '5': Rank.FIVE, '4': Rank.FOUR, '3': Rank.THREE, '2': Rank.TWO}
+            if not ranks.__contains__(string):
+                raise ValueError(f'could not resolve rank {string}')
+            return ranks[string]
+
+        raise ValueError('String is too long or too short to be a card')
 
     def __str__(self):
         return str(self.rank.to_short_string()) + str(self.suit.to_unicode())
