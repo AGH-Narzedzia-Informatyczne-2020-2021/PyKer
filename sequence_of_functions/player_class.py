a174236c0915512c56e6bd034959ac05bfd1e505
zarebajakub <73066603+zarebajakub@users.noreply.github.com>
2020-11-24 16:23:03 +0100

    Update player_class.py

--- sequence_of_functions/player_class.py
+++ sequence_of_functions/player_class.py
@@ -4,6 +4,6 @@ class Player:
         self.money = 100
 
         
-class Bot:
+class Bots:
     def __init__(self):
         self.name = input("Wprowadz imie bota: ") #do zrobienia dynamiczną numeracje przy wpisywaniu
